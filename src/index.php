e309a254f04139713e71b01ac71afc288947fdc8
Julien <julienbroens@gmail.com>
2020-03-02 16:53:51 +0100

    test requetes sql

--- src/index.php
+++ src/index.php
@@ -1,2 +1,23 @@
-<?php echo  "hello";
 
+
+<?php echo  "hello " . "<br>";
+
+require('config.php');
+
+
+
+// $sql = "SELECT name FROM users";
+// $res_select = $db_connect->query($sql);
+// $resLine = $res_select->fetchAll();
+
+
+
+// $resLine représente une ligne de la table de résultats de la requête
+// C'est un tableau indexé (par les noms ET indices des champs par défaut)
+
+//echo $resLine['champ1'];
+//echo $resLine[0]; // même valeur
+//echo $resLine[0]["name"];
+// foreach($resLine as $value) {
+//     echo $value["name"] . "<br>";
+// }
