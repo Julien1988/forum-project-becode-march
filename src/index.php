27a1294a76d7de0d0cad4fc19f0d0beaa31185ec
Julien <julienbroens@gmail.com>
2020-03-03 15:29:27 +0100

    test des jointures ok

--- src/index.php
+++ src/index.php
@@ -1,23 +1,30 @@
 
 
 <?php echo  "hello " . "<br>";
+?>
+<br>
+<?php
 
 require('config.php');
 
+$sql = "SELECT users.name, messages.content FROM users, messages ";
+$res_select = $db_connect->query($sql);
+$resLine = $res_select->fetchall();
 
 
-// $sql = "SELECT name FROM users";
-// $res_select = $db_connect->query($sql);
-// $resLine = $res_select->fetchAll();
+//var_dump($resLine[0]); // même valeur
+
+echo $resLine[0]["name"];
+
+?>
+<br>
+<br>
+<?php
+
+echo $resLine[0]["content"];
 
 
 
-// $resLine représente une ligne de la table de résultats de la requête
-// C'est un tableau indexé (par les noms ET indices des champs par défaut)
 
-//echo $resLine['champ1'];
-//echo $resLine[0]; // même valeur
-//echo $resLine[0]["name"];
-// foreach($resLine as $value) {
-//     echo $value["name"] . "<br>";
-// }
+
+
