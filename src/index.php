f3d282a00fb8d278ba78c074fc7bdc7332f15dc0
Julien <julienbroens@gmail.com>
2020-03-04 09:21:46 +0100

    first commit push

--- src/index.php
+++ src/index.php
@@ -7,22 +7,27 @@
 
 require('config.php');
 
-$sql = "SELECT users.name, messages.content FROM users, messages ";
+// $sql = "SELECT users.name, messages.content FROM users, messages WHERE users.idUsers = messages.Users_idUsers ";
+$sql = "SELECT users.name, messages.content FROM users INNER JOIN messages ON users.idUsers = messages.Users_idUsers ";
 $res_select = $db_connect->query($sql);
 $resLine = $res_select->fetchall();
 
+// Boucle de récupération des messages et du nom de l'auteur
 
-//var_dump($resLine[0]); // même valeur
+$resLineCount = count($resLine);
 
-echo $resLine[0]["name"];
-
+for ($i = 0; $i < $resLineCount; $i++) {
+    echo $resLine[$i]["content"];
+    ?>
+<br>
+<br>
+<?php
+    echo $resLine[$i]["name"];
 ?>
 <br>
 <br>
 <?php
-
-echo $resLine[0]["content"];
-
+}
 
 
 
